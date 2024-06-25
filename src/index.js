883a9ce30845975149317c10b939ebe92bb1ab4b
Mabel Atombo <mabelatombo@gmail.com>
2024-06-25 07:38:52 +0000

    css styling

---
+++ src/index.js
@@ -0,0 +1,4 @@
+let name = prompt( "What is your name?");
+let age = prompt ("What is your age?");
+let email= prompt ( "What is your email?");
+alert(`Hi ${name},you are ${age} years old and we will keep in touch via your email "${email}"👍👍.`);  

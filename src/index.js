6757234450a51a2e4d800f90e2db4d4e82f714ed
Mabel Atombo <mabelatombo@gmail.com>
2024-07-03 11:52:02 +0000

    commit all changes

--- src/index.js
+++ src/index.js
@@ -1,4 +1,30 @@
-let name = prompt( "What is your name?");
-let age = prompt ("What is your age?");
-let email= prompt ( "What is your email?");
-alert(`Hi ${name},you are ${age} years old and we will keep in touch via your email "${email}"👍👍.`);  
+// function userDetails(){
+//  let name = prompt( "What is your name?");
+// let age = prompt ("What is your age?");
+// let email= prompt ( "What is your email?");
+// alert(`Hi ${name},you are ${age} years old and we will keep in touch via your email "${email}"👍👍.`);  
+// }
+
+// userDetails{
+// h1
+// }
+let topic = document.getElementById("topic");
+topic.style.backgroundColor= "purple";
+topic.innerHTML ="I'm done learning JS";
+
+let paragraph = document.getElementById("types");
+paragraph.style.backgroundColor= "yellow";
+paragraph.innerHTML="I now know the datatypes";
+paragraph.style.padding="10px";
+paragraph.style.textAlign= "center";
+paragraph.style.borderColor = "red";
+
+let corn =document.getElementById("conditionals");
+corn.style.backgroundColor = "blue";
+corn.style.paddingBlock = "20px";
+corn.innerHTML ="I can condition my elements";
+
+let learn = document.getElementById("learning");
+learn.style.backgroundColor ="pink";
+learn.innerHTML ="My DOM is Doming"
+
